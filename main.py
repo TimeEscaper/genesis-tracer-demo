2ab2b88b1211228a69cae37c3b61461ffc0e8630
Timur Akhtyamov <rentgeny05@gmail.com>
2025-08-22 10:39:18 +0300

    Staged updated to the model and script; original backup added

--- main.py
+++ main.py
@@ -2,6 +2,22 @@ import torch
 import numpy as np
 import genesis as gs
 
+
+class DiffDrive:
+
+    def __init__(self, r: float, l: float):
+        self._r = r
+        self._l = l
+
+    def __call__(self, v: float, w: float):
+        c1 = 1. / (2. * self._r)
+        c2 = 2. * v
+        c3 = w * self._l
+        w_r = c1 * (c2 + c3)
+        w_l = c1 * (c2 - c3)
+        return np.array([w_r, w_l])
+
+
 if torch.cuda.is_available():
     print("GPU available")
     gs.init(backend=gs.gpu)
@@ -9,9 +25,13 @@ else:
     print("GPU not available")
     gs.init(backend=gs.cpu)
 
+controller = DiffDrive(r=0.0605, l=0.341)
+
 scene = gs.Scene(
     sim_options = gs.options.SimOptions(
         dt = 0.01,
+        substeps=10,
+        requires_grad=False
     ),
     show_viewer = True,
 )
@@ -33,16 +53,19 @@ tracer = scene.add_entity(
 scene.build()
 
 jnt_names = [
-    'left_wheel',
     'right_wheel',
+    'left_wheel',
 ]
 dofs_idx = [tracer.get_joint(name).dof_idx_local for name in jnt_names]
 
-for _ in range(100000):
+action = controller(v=1.5, w=np.deg2rad(0.))
+
+for i in range(100):
     tracer.control_dofs_velocity(
-        # Minus sign is a hack to achieve forward motion
-        # Probably wheel joint should be rotated to fix this
-        np.array([10.0, 10.0]),
+        np.array([action[0], action[1]]),
         dofs_idx,
     )
     scene.step()
+
+print(action)
+print(f"Pose: {tracer.get_pos()}")
