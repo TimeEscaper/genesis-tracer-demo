f718ac5c892d64d87de6ff72959bf98bbb178a04
Timur Akhtyamov <rentgeny05@gmail.com>
2025-08-24 12:17:03 +0300

    Beluga added

--- main.py
+++ main.py
@@ -28,6 +28,9 @@ else:
 controller = DiffDrive(r=0.0605, l=0.341)
 
 scene = gs.Scene(
+    rigid_options=gs.options.RigidOptions(
+        enable_self_collision=False,
+    ),
     sim_options = gs.options.SimOptions(
         dt = 0.01,
         substeps=10,
@@ -42,7 +45,9 @@ plane = scene.add_entity(
 
 tracer = scene.add_entity(
     gs.morphs.URDF(
-        file  = './urdf/tracer/tracer.urdf',
+        file  = './urdf/akula/akula.urdf',
+        decompose_robot_error_threshold=0.4,
+        decimate_aggressiveness=0,
         # convexify=False,
         # decimate=False,
         pos   = (0.0, 0.0, 0.15),
@@ -50,22 +55,44 @@ tracer = scene.add_entity(
     ),
 )
 
+scene.add_entity(
+    morph=gs.morphs.Box(
+        pos = (2., 0.0, 0.),
+        euler = (0, 0, 0),
+        size = (1., 1., 1.),
+        fixed = True,
+    ),
+    surface=gs.surfaces.Rough(
+                    diffuse_texture=gs.textures.ColorTexture(
+                        color=(1.0, 0.5, 0.5),
+                    ),
+                ),
+)
+
+camera = scene.add_camera()
+
+bumper = gs.sensors.RigidContactForceGridSensor(tracer)
+
 scene.build()
 
+# camera.attach()
+
 jnt_names = [
-    'right_wheel',
-    'left_wheel',
+    'R_wheel_joint',
+    'L_wheel_joint',
 ]
 dofs_idx = [tracer.get_joint(name).dof_idx_local for name in jnt_names]
 
 action = controller(v=1.5, w=np.deg2rad(0.))
 
-for i in range(100):
+for i in range(1000000):
     tracer.control_dofs_velocity(
         np.array([action[0], action[1]]),
         dofs_idx,
     )
     scene.step()
+    grid_data = bumper.read()
+    print(grid_data)
 
 print(action)
 print(f"Pose: {tracer.get_pos()}")
